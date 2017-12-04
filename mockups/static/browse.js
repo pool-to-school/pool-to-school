32b69bdddb02d485e72a8d05f627fc33789da99c
micah <mynatt@hawaii.edu>
2017-12-04 11:10:33 -1000

    - updated browse a bit - farewell DS_Store

--- mockups/static/browse.js
+++ mockups/static/browse.js
@@ -3,67 +3,45 @@ $(document).ready(()=>{
 	items = $(".browse.items");
 	items.children().each(function() {
 		let item = $(this);
-		let interests = item.find(".interest.details");
-		let interests_labels = interests.children();
-		let schedule = item.find(".schedule.details");
-		let location = item.find(".location.details");
+		let details = {
+			interest: item.find(".interest.details"),
+			schedule: item.find(".schedule.details"),
+			location: item.find(".location.details"),
+		};
 		let button = {
 			interest: item.find(".interest.button"),
 			schedule: item.find(".schedule.button"),
-			location: item.find(".location.button")
+			location: item.find(".location.button"),
 		};
 
-		// interests.hide();
-		// schedule.hide();
-		// location.hide();
+		let detailsToggle = function(field) {
+			return function() {
+				if (button[field].hasClass('active')) {
+					button[field].removeClass('active');
+				}
+				else {
+					button[field].addClass('active');
+				}
 
-		// console.log(item);
-		// console.log(interests);
-		// console.log(schedule);
-		// console.log(location);
+				details[field].transition({
+					animation: "fade down",
+				});
+			}
+		}
 
-		button.interest.on('click', ()=>{
-			
-			if (button.interest.hasClass('active'))
-				button.interest.removeClass('active');
-			else
-				button.interest.addClass('active');
-
-			interests.transition({
-				animation: "fade down",
-			});
-		});
-		button.schedule.on('click', ()=>{
-			if (button.schedule.hasClass('active'))
-				button.schedule.removeClass('active');
-			else
-				button.schedule.addClass('active');
-
-			schedule.transition({
-				animation: "fade down",
-			});
-		});
-		button.location.on('click', ()=>{
-			if (button.location.hasClass('active'))
-				button.location.removeClass('active');
-			else
-				button.location.addClass('active');
-
-			location.transition({
-				animation: "fade down",
-			});
-		});
+		for (field in details) {
+			button[field].on('click', detailsToggle(field));
+		}
 	});
 
 	menu = $(".category.menu");
 	menu.children().each(function() {
 		let item = $(this);
-		console.log(this);
-		item.on('click', () => {
+		item.on('click', function() {
 			menu.children().removeClass('active');
 			item.addClass('active');
 		});
 	});
 
-	console.log(menu);
+	// console.log(menu);
 });
